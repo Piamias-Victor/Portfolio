21cf67a3d3b08483fc966f9fe7cc36fb07eb7f88
ChocoDev23 <chocowalletdev@proton.me>
2022-11-19 10:35:18 +0100

    Head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
+import Head from "next/head";
 import { About } from "../src/components/about";
 import { Card } from "../src/components/card";
 import { Footer } from "../src/components/layout/footer";
 import { Header } from "../src/components/layout/header";
 import { ProjectSection } from "../src/components/project";
 import { Skills } from "../src/components/skills";
+import { ChildrenProps } from "../src/react/props";
 
 
 
 export default function Home() {
-  return <>
+  return <Wrapper>
     <div id='top' />
     <Header />
     <div className="h-[50px]" />
@@ -23,5 +25,33 @@ export default function Home() {
     </main>
     <div className="h-[50px]" />
     <Footer />
+  </Wrapper>
+}
+
+
+function Wrapper(props: ChildrenProps) {
+
+  const { children } = props
+
+  return <>
+    <Head>
+      <title>Piamias Victor</title>
+      <meta name="referrer" content="no-referrer" />
+      <meta name="application-name" content="Piamias Victor" />
+      <meta
+        name="description"
+        content="Build to learn, learn to buildy"
+      />
+      <meta name="color-scheme" content="dark light" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=5, viewport-fit=cover"
+      />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="white" />
+      <link rel="icon" type="image/svg+xml" href="/memoji.png" />
+      <link rel="alternate icon" href="/memoji.png" />
+    </Head>
+    {children}
   </>
 }
